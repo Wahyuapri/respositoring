5692d6d14e84446f9720f5ad422b7469f9167fc2
WAHYU APRI JOGI BANCIN <127834640+Wahyuapri@users.noreply.github.com>
2023-05-05 20:06:52 +0700

    Update strlen.php

--- respositoring/strlen.php
+++ respositoring/strlen.php
@@ -4,6 +4,6 @@ echo strlen("Hello World!")."<br>";
 echo str_word_count("Hello World!")."<br>";
 echo strrev("Hello World!")."<br>";
 echo strpos("Hello World!","or")."<br>";
-echo str_replace("Hello world","Priado Siregar","Hello world");
+echo str_replace("Hello world","WAHYU APRI JOGI BANCIN","Hello world");
 
 ?>
